a432557dc0a3d3197b3d0437dbdcb6fc563f2412
Jacob Engstrom <jakeengstrom3@gmail.com>
2019-12-04 14:40:01 -0800

    Put data from web-service onto graph Added 'About' section Added a pretty background

--- main.js
+++ main.js
@@ -1,69 +1,42 @@
 const BASE_URL = "https://zagster-service.herokuapp.com"
 
-$(updateGraph)
 $(updateView)
-/*
-Stuff done in class 11/25
-
-$(greeter("Jake"))
-
-add(2, 3);
-
-function greeter(name){
-  alert("Welcome to " + name + "'s data visualization")
-}
-
-function add(num1, num2){
-  answer = num1 + num2;
-  console.log("The answer is: " + answer);
-  return answer;
-}
-
-var person = {name: "Jake", age: 18, car: {model: "Toyota", year: 1997} }
-console.log("My name is: " + person.name)
-console.log("My age is: " + person.age)
-console.log("My car model: " + person.car.model)
-
-if(person.age > 50){
-  alert("u old")
-}
-
-var data = {"2016":[{"9":220},{"10":141},{"11":89},{"12":16}]}
-var year_list = data[2016]
-console.log(year_list[0][9])
-console.log(year_list[1][10])
-console.log(year_list[2][11])
-console.log(year_list[3][12])
-
-*/
-
-let years = []
-let months2016 = []
-let months2017 = []
-let months2018 = []
 
 function updateView() {
   $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
 
-  $.when ($.getJSON(BASE_URL + "/rides/count/per_month", perYear),
+  $.when ($.getJSON(BASE_URL + "/rides/count/per_month", perMonth),
     ).then(updateGraph);
 }
 
-function perYear(data){
-  for(var index = 0, month = 9; index <= 3, month <=12; ++index, ++month){
-    months2016.push(data[2016] [index] [month])
-  }
-  console.log("2016 data by months is easy " + months2016)
+let dataByMonths = []
 
-  for(var index = 0, month = 1; index <= 11, month <=12; ++index, ++month){
-    months2017.push(data[2017] [index] [month])
+function perMonth(data){ //Loads up dataByMonths with data so that each month is represented by an index in dataByMonths, so that the 0th index is January and so on.
+  for(var index = 0, month = 9; index <= 3, month <=12; ++index, ++month){ // for 2016
+    dataByMonths[month - 1] = data[2016] [index] [month] // month - 1 so that the indexes of databyMonths represents the month
   }
-  console.log("2017 data by months is easy " + months2017)
 
-  for(var index = 0, month = 1; index <= data.length, month <=10; ++index, ++month){
-    months2018.push(data[2018] [index] [month])
+  console.log(dataByMonths)
+
+  for(var index = 0, month = 1; index <= 11, month <=12; ++index, ++month){ // for 2017
+    if(dataByMonths[month - 1] == null){
+      dataByMonths[month - 1] = data[2017] [index] [month]
+    }else{
+      dataByMonths[month - 1] += data[2017] [index] [month]
+    }
   }
-  console.log("2018 data by months is easy " + months2018)
+
+  console.log(dataByMonths)
+
+  for(var index = 0, month = 1; index <= data.length, month <=10; ++index, ++month){ // for 2018
+    if(dataByMonths[month - 1] == null){
+      dataByMonths[month - 1] = data[2018] [index] [month]
+    }else{
+      dataByMonths[month - 1] += data[2018] [index] [month]
+    }  
+  }
+
+  console.log(dataByMonths)
 }
 
 function updateRideCount(data) {
@@ -71,10 +44,6 @@ function updateRideCount(data) {
   $("h2#rideCount").html("Total Number of Rides: " + numberOfRides)
   console.log(numberOfRides)
 }
-
-
-
-
 
 function updateGraph(){
   var ctx = document.getElementById('myChart').getContext('2d');
@@ -86,14 +55,13 @@ function updateGraph(){
       data: {
           labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
           datasets: [{
-              label: 'My First dataset',
-              backgroundColor: 'rgb(255, 99, 132)',
+              label: '',
+              backgroundColor: 'rgb(0, 99, 132)',
               borderColor: 'rgb(255, 99, 132)',
-              data: [0, 10, 5, 2, 20, 30, 45]
+              data: dataByMonths
           }]
       },
-
       // Configuration options go here
-      options: {}
+      options:{}
   });
-}
+} 
